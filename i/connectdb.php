4a4848fac1bfe519748f12806d3768c9fe1716fd
Rabbitoneplayer <rabbitoneplayer@gmail.com>
2026-02-17 09:08:56 +0700

    Add files via upload

---
+++ i/connectdb.php
@@ -0,0 +1,10 @@
+<?php 
+				$host = "localhost";
+				$user = "root";
+				$pwd = "";
+				$db = "4074db";
+				$conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+				mysqli_query($conn,"SET NAMES utf8");
+
+
+?>
