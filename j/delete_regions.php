4823df5add055e10fa1a64e865bb29f37aaeda20
Rabbitoneplayer <rabbitoneplayer@gmail.com>
2026-02-17 11:54:37 +0700

    Update delete_regions.php

--- j/delete_regions.php
+++ j/delete_regions.php
@@ -2,7 +2,7 @@
 
   <?php
   include_once("connectdb.php");
-  $id = $_GET[id];
+  $id = $_GET['id'];
   $sql = "DELETE FROM `regions` WHERE r_id='{$id}'";
   mysqli_query($conn , $sql) or die ("ลบไม่ได้");
 
