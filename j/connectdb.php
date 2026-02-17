d9b57f55e855429dbde43a1da181a5782f1fb4a2
Rabbitoneplayer <rabbitoneplayer@gmail.com>
2026-02-17 11:11:01 +0700

    Update connectdb.php

--- j/connectdb.php
+++ j/connectdb.php
@@ -1,10 +1,11 @@
 <?php 
 				$host = "localhost";
-				$user = "root";
-				$pwd = "";
-				$db = "4074db";
+				$user = "Admin";
+				$pwd = "Password";
+				$db = "4167db";
 				$conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 				mysqli_query($conn,"SET NAMES utf8");
 
 
 ?>
+
